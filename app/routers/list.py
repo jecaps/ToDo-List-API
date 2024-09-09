4e432a68c56a1c514481deb4d14258d63c5abb84
Jerika Kanz <jerikanz@jkanz.de>
2024-09-09 16:31:01 +0200

    crud_operations_for_lists deleting lists endpoint

--- app/routers/list.py
+++ app/routers/list.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.orm import Session
 
 from app.database import get_db
@@ -13,7 +13,7 @@ def create_list(list: ListCreate, db: Session = Depends(get_db)) -> ListWithoutT
     new_list = List(**list.model_dump())
 
     if new_list.title == "":    
-        raise HTTPException(status_code=400, detail="Title is required.")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Title is required.")
 
     db.add(new_list)
     db.commit()
@@ -39,7 +39,7 @@ def read_list(id: int, db: Session = Depends(get_db)) -> ListSchema:
     list = db.query(List).filter(List.id == id).first()
 
     if list is None:
-        raise HTTPException(status_code=404, detail=f"List with id no. {id} not found.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"List with id no. {id} not found.")
     
     return list
 
@@ -50,9 +50,22 @@ def update_list(id: int, list: ListCreate, db: Session = Depends(get_db))  -> Li
     list_to_update = list_query.first()
 
     if list_to_update is None:
-        raise HTTPException(status_code=404, detail=f"List with id no. {id} does not exist.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"List with id no. {id} does not exist.")
 
     list_query.update(list.model_dump())
     db.commit()
 
     return list_query.first()
+
+
+@router.delete("/{id}")
+def delete_list(id: int, db: Session = Depends(get_db)):
+    list = db.query(List).filter(List.id == id).first()
+
+    if list is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"List with id no. {id} does not exist.")
+
+    db.delete(list)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
