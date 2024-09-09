09fb58419b4cdcff024da44964305efde53a48ae
Jerika Kanz <jerikanz@jkanz.de>
2024-09-09 17:17:03 +0200

    crud_operations_for_lists refactoring endpoints

--- app/routers/list.py
+++ app/routers/list.py
@@ -23,13 +23,15 @@ def create_list(list: ListCreate, db: Session = Depends(get_db)) -> ListWithoutT
 
 
 @router.get("/")
-def read_lists(skip: int = 0, limit: int = 10, order: str = "desc", db: Session = Depends(get_db)) -> list[ListSchema]:
-    if order == "desc":
-        lists = db.query(List).order_by(List.updated_at.desc()).offset(skip).limit(limit).all()
-    elif order == "asc":
-        lists = db.query(List).order_by(List.updated_at.asc()).offset(skip).limit(limit).all()
-    else:
-        lists = db.query(List).offset(skip).limit(limit).all()
+def read_lists(skip: int = 0, limit: int = 10, sort_by: str = "desc", db: Session = Depends(get_db)) -> list[ListSchema]:
+    query = db.query(List)
+
+    if sort_by == "asc":
+        query = query.order_by(List.updated_at.asc())
+    elif sort_by == "desc":
+        query = query.order_by(List.updated_at.desc())
+
+    lists = query.offset(skip).limit(limit).all()
 
     return lists
 
@@ -50,22 +52,24 @@ def update_list(id: int, list: ListCreate, db: Session = Depends(get_db))  -> Li
     list_to_update = list_query.first()
 
     if list_to_update is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"List with id no. {id} does not exist.")
-
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"List with id no. {id} not found.")
+    
     list_query.update(list.model_dump())
     db.commit()
+    db.refresh(list_to_update)
 
-    return list_query.first()
+    return list_to_update
 
 
 @router.delete("/{id}")
-def delete_list(id: int, db: Session = Depends(get_db)):
-    list = db.query(List).filter(List.id == id).first()
+def delete_list(id: int, db: Session = Depends(get_db)) -> Response:
+    list_query = db.query(List).filter(List.id == id)
+    list = list_query.first()
 
     if list is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"List with id no. {id} does not exist.")
-
-    db.delete(list)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"List with id no. {id} not found.")
+    
+    list_query.delete()
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
