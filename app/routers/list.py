6a3864cfc78a4d4b3d3c0652c1df5d8c99d5fa97
Jerika Kanz <jerikanz@jkanz.de>
2024-09-12 13:31:20 +0200

    crud_operations_for_lists refactored endpoints

--- app/routers/list.py
+++ app/routers/list.py
@@ -1,20 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
 
 from app.database import get_db
-from app.models import List
-from app.schemas import List as ListSchema
-from app.schemas import ListCreate, ListWithoutTodos
+from app.models import ListDB
+from app.schemas import List, ListCreate
 
 router = APIRouter(prefix="/lists", tags=["Lists"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_list(list: ListCreate, db: Session = Depends(get_db)) -> ListWithoutTodos:
-    new_list = List(**list.model_dump())
-
-    if new_list.title == "":    
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Title is required.")
-
+def create_list(list: ListCreate, db: Session = Depends(get_db)) -> List:
+    new_list = ListDB(**list.model_dump())
     db.add(new_list)
     db.commit()
     db.refresh(new_list)
@@ -23,38 +19,37 @@ def create_list(list: ListCreate, db: Session = Depends(get_db)) -> ListWithoutT
 
 
 @router.get("/")
-def read_lists(skip: int = 0, limit: int = 10, sort_by: str = "desc", db: Session = Depends(get_db)) -> list[ListSchema]:
-    query = db.query(List)
+def read_lists(skip: int = 0, limit: int = 10, sort_by: str = "desc", db: Session = Depends(get_db))   -> list[List]:
+    lists_db = db.query(ListDB)
 
     if sort_by == "asc":
-        query = query.order_by(List.updated_at.asc())
+        lists_db = lists_db.order_by(ListDB.updated_at.asc())
     elif sort_by == "desc":
-        query = query.order_by(List.updated_at.desc())
+        lists_db = lists_db.order_by(ListDB.updated_at.desc())
 
-    lists = query.offset(skip).limit(limit).all()
+    lists = lists_db.offset(skip).limit(limit).all()
 
     return lists
 
 
 @router.get("/{id}")
-def read_list(id: int, db: Session = Depends(get_db)) -> ListSchema:
-    list = db.query(List).filter(List.id == id).first()
-
-    if list is None:
+def read_list(id: int, db: Session = Depends(get_db)) -> List:
+    try:
+        list_db = db.query(ListDB).filter(ListDB.id == id).one()
+        return list_db
+    except NoResultFound:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"List with id no. {id} not found.")
-    
-    return list
 
 
 @router.put("/{id}")
-def update_list(id: int, list: ListCreate, db: Session = Depends(get_db))  -> ListSchema | None:
-    list_query = db.query(List).filter(List.id == id)
-    list_to_update = list_query.first()
-
+def update_list(id: int, list: ListCreate, db: Session = Depends(get_db))  -> List:
+    list_db = db.query(ListDB).filter(ListDB.id == id)
+    list_to_update = list_db.first()
+        
     if list_to_update is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"List with id no. {id} not found.")
-    
-    list_query.update(list.model_dump())
+        
+    list_db.update(list.model_dump())
     db.commit()
     db.refresh(list_to_update)
 
@@ -63,13 +58,12 @@ def update_list(id: int, list: ListCreate, db: Session = Depends(get_db))  -> Li
 
 @router.delete("/{id}")
 def delete_list(id: int, db: Session = Depends(get_db)) -> Response:
-    list_query = db.query(List).filter(List.id == id)
-    list = list_query.first()
+    list_db = db.query(ListDB).filter(ListDB.id == id).one_or_none()
 
-    if list is None:
+    if list_db is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"List with id no. {id} not found.")
     
-    list_query.delete()
+    db.delete(list_db)
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
