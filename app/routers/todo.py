a21c9da915033667be45eea7cc119e4599d46a10
Jerika Kanz <jerikanz@jkanz.de>
2024-09-13 14:24:54 +0200

    crud_operations_todo implement get_todo

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session  
+from sqlalchemy.orm.exc import NoResultFound
 
 from app.database import get_db
 from app.models import TodoDB
@@ -15,3 +16,12 @@ def create_todo(todo: TodoCreate, db: Session = Depends(get_db)) -> Todo:
     db.refresh(new_todo)
 
     return new_todo
+
+
+@router.get("/{todo_id}")
+def get_todo(todo_id: int, db: Session = Depends(get_db)):
+    try:
+        todo_db = db.query(TodoDB).filter(TodoDB.id == todo_id).one()
+        return todo_db
+    except NoResultFound:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Todo with id no. {todo_id} not found.")
