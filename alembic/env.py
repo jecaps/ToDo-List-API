98c25281bc7188a1f925d34e8e3a61276b81697d
Jerika Kanz <jerikanz@jkanz.de>
2024-10-28 11:18:01 +0100

    data_validation_and_sanitization put import to top

--- alembic/env.py
+++ alembic/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 
+from app.database import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -17,8 +18,6 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.database import Base
-from app.models import TodoDB
 
 target_metadata = Base.metadata
 
